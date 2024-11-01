ee2354882183846584ef9e47d44b6b980c4fde25
zhouyanye <left2004@outlook.com>
2024-11-01 11:11:32 +0800

    Remove all files

--- backend/src/main/java/com/blog/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.blog.repository;
-
-import com.blog.model.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
